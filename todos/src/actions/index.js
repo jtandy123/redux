6f721b989e9e1103ca80f70ae5dfa18e4ade8a3b
Andy Jiang <982071036@qq.com>
2019-01-06 01:31:01 +0800

    add actions

---
+++ todos/src/actions/index.js
@@ -0,0 +1,48 @@
+let nextTodoId = 0;
+
+/*
+ * action types
+ */
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+/*
+ * other constants
+ */
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+
+/*
+ * action creators  
+ */
+export function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    payload: {
+      id: nextTodoId++,
+      text
+    }
+  };
+}
+
+export function toggleTodo(id) {
+  return {
+    type: TOGGLE_TODO,
+    payload: {
+      id
+    }
+  };
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    payload: {
+      filter
+    }
+  };
+}
